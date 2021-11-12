49b8e169da39c66867359600dd5d3ff3ea8e9255
guangzhen.ju.o <guangzhen.ju.o@nio.com>
2021-11-12 17:37:11 +0800

    update config.js

--- config/config.js
+++ config/config.js
@@ -6,10 +6,10 @@ module.exports = {
   port: '8090',
   dist: './',
   logPath: './log',
-  before: function() {
+  before: function() { // 钩子函数 同理可加after钩子
     console.log('This is before hook')
   },
-  proxies: {
+  proxies: { // 代理相关接口 接口加签都在此处统一处理 甚至接口埋点也可以在此处
     juHe: {
       url: 'http://apis.juhe.cn/',
       signType: 'sha256',
@@ -19,5 +19,8 @@ module.exports = {
         }
       }
     }
+  },
+  logConfig: { // 日志相关设置
+
   }
 }
